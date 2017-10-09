776712f8422ff9e94c4fdf472a28c4109b1efb81
Chris Gonzales <cm.gonzales@yahoo.com>
2017-10-08 20:46:29 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function clickme(){
+    
+    variables();
+    
+    console.log(variables());
+    
+    }
+       
+     
+    function variables(){
+    
+      var mon = document.getElementById("month").selectedIndex;
+     
+      var month = document.getElementsByTagName("option")[mon].value;
+           
+      var date = document.getElementById("day").selectedIndex;
+      
+      var day = document.getElementsByTagName("option")[date].value;
+    
+      var point =  parseInt(month);
+    
+      return (point);
+    } 
